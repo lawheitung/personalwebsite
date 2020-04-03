eda8b469463c2c955e069b8fcc94b0545858981c
lawheitung <lawheitung@gmail.com>
2020-04-04 00:27:08 +0800

    removed Intro part

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from './Components/Navbar/Navbar';
 import JourneyList from './Components/Journeylist/JourneyList';
-import Intro from './Components/Intro/Intro';
 import QualityList from './Components/QualityList/QualityList';
 import FencingList from './Components/FencingList/FencingList'
 
